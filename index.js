b0f00b9ca2ae181221c573db7aa9852656d41e42
Nicholas A Christensen <nickachristensen@gmail.com>
2021-08-11 20:47:06 -0400

    commit tree c5e31ad8df064ee807aac0f6687d70c78a66460c

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiply) {
+    return function (num) {
+      return multiply * num;
+    }
+  }
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, whichDrivers) {
+    return whichDrivers(drivers);
+}
+
+// function mySubtract (number) {
+//     if (number > 11) {
+//       return number - 11;
+//     } else {
+//       return 11 - number;
+//     }
+//     return false
+//   }
+  
+//   console.log(mySubtract(100));
